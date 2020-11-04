43805065b4b245bd4e4c3752607860c7722fc798
Gunnar Große Hovest <gr-hovest@atb-bremen.de>
2020-11-04 15:33:34 +0100

    added new mail config parameters to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mvn clean install -Dygg.mail.sender.user="" -Dygg.mail.sender.password=""
+# Script to build CoatRack using Maven
+
+# Please note: 
+# - a CoatRack build requires some mail configuration parameters to be set
+# - the configuration values in this file are just dummy/fallback values for development builds
+
+mvn clean install -Dygg.mail.sender.user="" -Dygg.mail.sender.password="" -Dygg.mail.server.url="" -Dygg.mail.server.port=0
