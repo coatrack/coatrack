856ed3bb844360b51b7e59b51c7957d061fa236e
Gunnar Große Hovest <gr-hovest@atb-bremen.de>
2020-12-14 17:46:21 +0100

    run script now builds coatrack first if jar file is missing

--- run-admin-web-application.sh
+++ run-admin-web-application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to run the CoatRack web application (after it has been built using 'build.sh')
+# Script to run the CoatRack admin web application 
 
 # After startup, please use a web browser to access the application at http://localhost:8080
 
@@ -10,15 +10,15 @@
 
 COATRACK_JAR_FILE='spring-boot/admin/target/coatrack-admin-*.jar'
 
-
-if [ -f $COATRACK_JAR_FILE ]; then 
-
-	java -jar -Dygg.mail.sender.user="" -Dygg.mail.sender.password="" -Dygg.mail.server.url="" -Dygg.mail.server.port=0 $COATRACK_JAR_FILE
-
-else
-	echo "Error: CoatRack executable file '$COATRACK_JAR_FILE' could not be found."
-	echo "Please build CoatRack first before trying to run it, please use the script 'build.sh'."
-	exit
+if [ ! -f $COATRACK_JAR_FILE ]; then 
+	echo "--------------------------------------------------------------------------------"
+	echo " CoatRack executable file could not be found. Trying to build CoatRack first... "
+	echo "--------------------------------------------------------------------------------"
+	./build.sh
 fi
 
+echo "---------------------------------------"
+echo " Running CoatRack Admin application... "
+echo "---------------------------------------"
+java -jar -Dygg.mail.sender.user="" -Dygg.mail.sender.password="" -Dygg.mail.server.url="" -Dygg.mail.server.port=0 $COATRACK_JAR_FILE
 
