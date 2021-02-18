47df1a4ded74321b834a8ae1a353012eeee45ac2
Philip Reimer <reimer@atb-bremen.de>
2021-02-18 19:28:21 +0100

    closes #92

--- build-push-docker-image.sh
+++ build-push-docker-image.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-export MODULE_NAME=${1}
+MODULE_NAME=${1}
+MODULE_VERSION=${2}
+MODULE_DIR=${3}
 
-echo "building version $MVN_VERSION of module $MODULE_NAME"
+echo "building version $MODULE_VERSION of module $MODULE_NAME in $MODULE_DIR"
 
-docker build -t coatrack/$MODULE_NAME:$MVN_VERSION --build-arg MVN_VERSION=$MVN_VERSION --build-arg MODULE_NAME=$MODULE_NAME .
-docker push coatrack/$MODULE_NAME:$MVN_VERSION
+docker build -t coatrack/$MODULE_NAME:$MODULE_VERSION --build-arg MODULE_VERSION=$MODULE_VERSION --build-arg MODULE_NAME=$MODULE_NAME --build-arg MODULE_DIR=$MODULE_DIR .
+docker push coatrack/$MODULE_NAME:$MODULE_VERSION
