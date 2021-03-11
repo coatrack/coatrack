a0574bb25dd5f028c83f0ef5ad690087e2bf4264
Christoph Baier <cbaier@uni-bremen.de>
2021-03-11 19:05:05 +0100

    Improved names of times

--- spring-boot/proxy/src/test/java/ApiKeyValidityCheckerTest.java
+++ spring-boot/proxy/src/test/java/ApiKeyValidityCheckerTest.java
@@ -8,18 +8,21 @@ import org.springframework.http.ResponseEntity;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class ApiKeyValidityCheckerTest {
 
-    private final long oneDayInMillis = 1000 * 60 * 60 *24;
     private final String someValidValue = "someValidValue";
-    private ApiKeyValidityChecker checker = new ApiKeyValidityChecker();
+
     private ApiKey apiKey;
-    private Date tomorrow = new Date();
     private List<String> apiKeyValueList;
     private ResponseEntity<ApiKey> responseEntity;
+    private ApiKeyValidityChecker checker = new ApiKeyValidityChecker();
+
+    private final long oneDayInMillis = 1000 * 60 * 60 *24;
+    private Timestamp today = new Timestamp(System.currentTimeMillis());
+    private Timestamp tomorrow = new Timestamp(today.getTime() + oneDayInMillis);
+    private Timestamp yesterday = new Timestamp(today.getTime() - oneDayInMillis);
 
     @BeforeEach
     public void buildAWorkingSetup(){
@@ -32,7 +35,6 @@ public class ApiKeyValidityCheckerTest {
         apiKey = new ApiKey();
         apiKey.setKeyValue(someValidValue);
         apiKey.setDeletedWhen(null);
-        tomorrow.setTime(System.currentTimeMillis() + oneDayInMillis);
         apiKey.setValidUntil(tomorrow);
     }
 
@@ -54,18 +56,18 @@ public class ApiKeyValidityCheckerTest {
 
     @Test
     public void testIfDeletedKeyIsDenied(){
-        apiKey.setDeletedWhen(new Date(System.currentTimeMillis() - oneDayInMillis));
+        apiKey.setDeletedWhen(yesterday);
         assertFalse(checker.doesResultValidateApiKey(responseEntity, someValidValue));
     }
 
     @Test
     public void testIfExpiredKeyIsDenied(){
-        apiKey.setValidUntil(new Date(System.currentTimeMillis() - oneDayInMillis));
+        apiKey.setValidUntil(yesterday);
         assertFalse(checker.doesResultValidateApiKey(responseEntity, someValidValue));
     }
 
     @Test
-    public void testIfAKeyRejectedByAdminIsDenied(){
+    public void testIfAKeyWhichWasRejectedByAdminIsDenied(){
         apiKey = null;
         responseEntity = new ResponseEntity<>(apiKey, HttpStatus.OK);
         assertFalse(checker.doesResultValidateApiKey(responseEntity, someValidValue));
@@ -87,7 +89,7 @@ public class ApiKeyValidityCheckerTest {
     @Test
     public void testIfApiKeyIsDeniedBecauseAdminIsNotReachableForLongerThanOneHour(){
         responseEntity = null;
-        checker.setLastApiKeyValueListUpdate(new Timestamp(System.currentTimeMillis() - oneDayInMillis));
+        checker.setLastApiKeyValueListUpdate(yesterday);
         assertFalse(checker.doesResultValidateApiKey(responseEntity, someValidValue));
     }
 }
