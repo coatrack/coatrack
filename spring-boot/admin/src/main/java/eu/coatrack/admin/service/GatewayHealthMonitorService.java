66638dd641998d2f7a54b95cb0d23c74a1732757
Bruno Silva <silva@atb-bremen.de>
2021-05-21 13:42:49 +0100

    GatewayHealthMonitorService created so that there is no business logic on the Admin Controller, just the view

---
+++ spring-boot/admin/src/main/java/eu/coatrack/admin/service/GatewayHealthMonitorService.java
@@ -0,0 +1,66 @@
+package eu.coatrack.admin.service;
+
+import eu.coatrack.admin.model.repository.ProxyRepository;
+import eu.coatrack.api.Proxy;
+import eu.coatrack.api.ProxyStates;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GatewayHealthMonitorService {
+
+    @Value("${ygg.gateway-health-monitor.warning.threshold.minutes}")
+    private int warningThresholdInMinutes;
+
+    @Value("${ygg.gateway-health-monitor.critical.threshold.minutes}")
+    private int criticalThresholdInMinutes;
+
+    @Autowired
+    ProxyRepository proxyRepository;
+
+    public static class GatewayDataForTheGatewayHealthMonitor {
+        public String proxyId;
+        public String name;
+        public ProxyStates status;
+        public Long minutesPastSinceLastContact;
+        public boolean isMonitoringEnabled;
+    }
+
+    public ProxyStates gatewayHealthStatusSummary(List<GatewayDataForTheGatewayHealthMonitor> gatewayDataForTheGatewayHealthMonitorList){
+        List <GatewayDataForTheGatewayHealthMonitor> proxiesWithMonitoringActivatedList = gatewayDataForTheGatewayHealthMonitorList.stream().filter(proxy -> proxy.isMonitoringEnabled == true).collect(Collectors.toList());
+        if (proxiesWithMonitoringActivatedList.stream().anyMatch(proxy -> proxy.status == ProxyStates.CRITICAL))
+            return ProxyStates.CRITICAL;
+        else if(proxiesWithMonitoringActivatedList.stream().anyMatch(proxy -> proxy.status == ProxyStates.WARNING))
+            return ProxyStates.WARNING;
+        return ProxyStates.OK;
+    }
+
+    public List<GatewayDataForTheGatewayHealthMonitor> updateProxyInfoForGatewayHealthMonitor() {
+        List <GatewayDataForTheGatewayHealthMonitor> proxyDataForGatewayHealthMonitorList = new ArrayList<>();
+        List<Proxy> proxiesToBeChanged = proxyRepository.findAvailable();
+        proxiesToBeChanged.forEach((proxy) -> {
+            GatewayDataForTheGatewayHealthMonitor proxyDataForGatewayHealthMonitor = new GatewayDataForTheGatewayHealthMonitor();
+            proxyDataForGatewayHealthMonitor.proxyId = proxy.getId();
+            proxyDataForGatewayHealthMonitor.name = proxy.getName();
+            proxyDataForGatewayHealthMonitor.isMonitoringEnabled = proxy.isMonitoringEnabled();
+            if (proxy.getTimeOfLastSuccessfulCallToAdmin() != null && proxy.isMonitoringEnabled()) {
+                Long minutesPastSinceLastContact = Duration.between(proxy.getTimeOfLastSuccessfulCallToAdmin(), LocalDateTime.now()).toMinutes();
+                proxyDataForGatewayHealthMonitor.minutesPastSinceLastContact = minutesPastSinceLastContact;
+                if (minutesPastSinceLastContact > criticalThresholdInMinutes) {
+                    proxyDataForGatewayHealthMonitor.status = ProxyStates.CRITICAL;
+                } else if (minutesPastSinceLastContact > warningThresholdInMinutes) {
+                    proxyDataForGatewayHealthMonitor.status = ProxyStates.WARNING;
+                } else proxyDataForGatewayHealthMonitor.status = ProxyStates.OK;
+            } else proxyDataForGatewayHealthMonitor.status = ProxyStates.IGNORE;
+            proxyDataForGatewayHealthMonitorList.add(proxyDataForGatewayHealthMonitor);
+        });
+        return proxyDataForGatewayHealthMonitorList;
+    }
+}
