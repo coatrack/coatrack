4ed1ac34bf755aac3f5b8981ea716e5759a20baa
Bruno Silva <silva@atb-bremen.de>
2021-06-02 14:59:13 +0100

    Method that is always used added to the setup() method on the tests

--- spring-boot/admin/src/test/java/eu/coatrack/admin/service/GatewayHealthMonitorServiceTest.java
+++ spring-boot/admin/src/test/java/eu/coatrack/admin/service/GatewayHealthMonitorServiceTest.java
@@ -45,6 +45,7 @@ public class GatewayHealthMonitorServiceTest {
     public void setup() {
         MockitoAnnotations.initMocks(this);
         sampleList = Arrays.asList(createSampleProxy(true, "test1"), createSampleProxy(false, "test2"), proxySample);
+        when(proxyRepository.findAvailable()).thenReturn(sampleList);
         when(proxySample.isMonitoringEnabled()).thenReturn(true);
         when(proxySample.getName()).thenReturn("test3");
         ReflectionTestUtils.setField(gatewayHealthMonitorService, "gatewayHealthWarningThresholdInMinutes", 5);
@@ -53,35 +54,30 @@ public class GatewayHealthMonitorServiceTest {
 
     @Test
     public void ifAllGatewaysAreOkOrIgnore_ThenStatusSummaryShouldReturnOkState() {
-        when(proxyRepository.findAvailable()).thenReturn(sampleList);
         when(proxySample.getTimeOfLastSuccessfulCallToAdmin()).thenReturn(LocalDateTime.now());
         Assert.assertEquals(ProxyStates.OK, gatewayHealthMonitorService.calculateGatewayHealthStatusSummary(gatewayHealthMonitorService.getGatewayHealthMonitorData()));
     }
 
     @Test
     public void ifOneGatewayIsWarningAndOthersOKOrIgnore_ThenStatusSummaryShouldReturnWarningState() {
-        when(proxyRepository.findAvailable()).thenReturn(sampleList);
         when(proxySample.getTimeOfLastSuccessfulCallToAdmin()).thenReturn(LocalDateTime.now().minusMinutes(6));
         Assert.assertEquals(ProxyStates.WARNING, gatewayHealthMonitorService.calculateGatewayHealthStatusSummary(gatewayHealthMonitorService.getGatewayHealthMonitorData()));
     }
 
     @Test
     public void ifOneGatewayIsCritical_ThenStatusSummaryShouldReturnCriticalState() {
-        when(proxyRepository.findAvailable()).thenReturn(sampleList);
         when(proxySample.getTimeOfLastSuccessfulCallToAdmin()).thenReturn(LocalDateTime.now().minusMinutes(70));
         Assert.assertEquals(ProxyStates.CRITICAL, gatewayHealthMonitorService.calculateGatewayHealthStatusSummary(gatewayHealthMonitorService.getGatewayHealthMonitorData()));
     }
 
     @Test
     public void ifAllGatewaysNotConnected_ThenStatusSummaryShouldReturnNotConnectedState() {
-        when(proxyRepository.findAvailable()).thenReturn(sampleList);
         when(proxySample.getTimeOfLastSuccessfulCallToAdmin()).thenReturn(null);
         Assert.assertEquals(ProxyStates.NEVER_CONNECTED, gatewayHealthMonitorService.calculateGatewayHealthStatusSummary(gatewayHealthMonitorService.getGatewayHealthMonitorData()));
     }
 
     @Test
     public void listShouldBeOrganizedFirstByMonitoringEnabledAndThenByName() {
-        when(proxyRepository.findAvailable()).thenReturn(sampleList);
         List<String> gatewayNames = Arrays.asList("test1", "test3", "test2");
         Assert.assertEquals(gatewayNames,
                 gatewayHealthMonitorService.getGatewayHealthMonitorData()
