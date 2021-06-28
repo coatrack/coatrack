a6779b71059960826d2ce6e91d068ca0825fccc7
Bruno Silva <silva@atb-bremen.de>
2021-06-28 07:02:51 +0100

    Removed the method getCssClassForGui from the ProxyHealthStatus. So now, all is included in Frontend

--- api/src/main/java/eu/coatrack/api/ProxyHealthStatus.java
+++ api/src/main/java/eu/coatrack/api/ProxyHealthStatus.java
@@ -9,9 +9,9 @@ package eu.coatrack.api;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,19 +22,9 @@ package eu.coatrack.api;
 
 public enum ProxyHealthStatus {
 
-    CRITICAL("critical-state"),
-    WARNING("warning-state"),
-    OK("ok-state"),
-    IGNORE("ignore-state"),
-    NEVER_CONNECTED("");
-
-    private final String cssClassForGui;
-
-    ProxyHealthStatus(String cssClassForGui) {
-        this.cssClassForGui = cssClassForGui;
-    }
-
-    public String getCssClassForGui() {
-        return cssClassForGui;
-    }
+    CRITICAL(),
+    WARNING(),
+    OK(),
+    IGNORE(),
+    NEVER_CONNECTED();
 }
