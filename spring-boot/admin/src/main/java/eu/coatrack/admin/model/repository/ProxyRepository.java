46c566ae6048443edf80627bb5fbf0c91f58cf27
Christoph Baier <cbaier@uni-bremen.de>
2021-08-12 13:26:37 +0200

    Re-implemented the previously commented out security restriction of the findById() method for proxies

--- spring-boot/admin/src/main/java/eu/coatrack/admin/model/repository/ProxyRepository.java
+++ spring-boot/admin/src/main/java/eu/coatrack/admin/model/repository/ProxyRepository.java
@@ -30,6 +30,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 
@@ -43,7 +44,7 @@ public interface ProxyRepository extends PagingAndSortingRepository<Proxy, Strin
     @PostFilter("filterObject.owner != null and filterObject.owner.username == authentication.name")
     List<Proxy> findByName(@Param("name") String name);
 
-    //@PreAuthorize("#id == authentication.name") TODO security feature needs to be re-implemented
+    @PostAuthorize("returnObject.get().owner.username == authentication.principal")
     Optional<Proxy> findById(@Param("id") String id);
 
     @PostFilter("filterObject.owner != null and filterObject.owner.username == authentication.name")
