ff68380785317f7d74532c38bf32fb43f42c27a4
Christoph Baier <cbaier@uni-bremen.de>
2021-09-03 08:46:54 +0200

    Removed redundant import from YggAdminApplication.

--- spring-boot/admin/src/main/java/eu/coatrack/admin/YggAdminApplication.java
+++ spring-boot/admin/src/main/java/eu/coatrack/admin/YggAdminApplication.java
@@ -23,7 +23,6 @@ package eu.coatrack.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
