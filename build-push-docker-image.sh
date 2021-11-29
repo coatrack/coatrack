26fb8110236972eeb8bdbec2e8ea114821e8c9f6
Christoph Baier <baier@atb-bremen.de>
2021-11-29 15:05:10 +0100

    Admin docker image now contains proxy jar file.

--- build-push-docker-image.sh
+++ build-push-docker-image.sh
@@ -6,5 +6,10 @@ MODULE_DIR=${3}
 
 echo "building version $MODULE_VERSION of module $MODULE_NAME in $MODULE_DIR"
 
-docker build -t coatrack/$MODULE_NAME:$MODULE_VERSION --build-arg MODULE_VERSION=$MODULE_VERSION --build-arg MODULE_NAME=$MODULE_NAME --build-arg MODULE_DIR=$MODULE_DIR .
+if [ "$MODULE_NAME" == "coatrack-admin" ]; then
+  docker build -t coatrack/$MODULE_NAME:$MODULE_VERSION --build-arg MODULE_VERSION=$MODULE_VERSION --build-arg MODULE_NAME=$MODULE_NAME --build-arg MODULE_DIR=$MODULE_DIR -f ./Dockerfile-Admin .
+else
+  docker build -t coatrack/$MODULE_NAME:$MODULE_VERSION --build-arg MODULE_VERSION=$MODULE_VERSION --build-arg MODULE_NAME=$MODULE_NAME --build-arg MODULE_DIR=$MODULE_DIR .
+fi
+
 docker push coatrack/$MODULE_NAME:$MODULE_VERSION
