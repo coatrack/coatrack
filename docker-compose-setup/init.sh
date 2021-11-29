f3e93d71fb40900b6725962c7f0ac9d623327082
Christoph Baier <baier@atb-bremen.de>
2021-11-29 16:03:05 +0100

    Local proxy jar is now used to easy-coatrack-deployment feature.

--- docker-compose-setup/init.sh
+++ docker-compose-setup/init.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 cd "$(dirname "$0")/.." || exit
-mvn package -DskipTests
-docker run --rm -dit -v coatrack-admin:/home --name coatrack-admin ubuntu
-docker cp ./spring-boot/proxy/target/coatrack-proxy-2.0.0-SNAPSHOT.jar coatrack-admin:/home
-docker stop coatrack-admin
 
 docker run --rm -d -p 5432:5432 \
   --name coatrack-postgres \
