84f0e07f5be403a3be70d6012c05eacc4bfb026a
Christoph Baier <baier@atb-bremen.de>
2021-11-30 16:31:18 +0100

    Introduced environment variables.

--- docker-compose-setup/init.sh
+++ docker-compose-setup/init.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-cd "$(dirname "$0")/.." || exit
+cd "$(dirname "$0")" || exit
+export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+cd ..
+
 mvn package -DskipTests
-docker run --rm -dit -v coatrack-admin:/home --name coatrack-admin ubuntu
-docker cp ./spring-boot/proxy/target/coatrack-proxy-2.0.0-SNAPSHOT.jar coatrack-admin:/home
-docker stop coatrack-admin
+docker run --rm -dit -v "${ADMIN_VOLUME}":/home --name "${ADMIN_CONTAINER_NAME}" ubuntu
+docker cp ./spring-boot/proxy/target/coatrack-proxy-"${TAG}".jar "${ADMIN_CONTAINER_NAME}":/home
+docker stop "${ADMIN_CONTAINER_NAME}"
 
 docker run --rm -d -p 5432:5432 \
-  --name coatrack-database \
-  -v coatrack-database-volume:/var/lib/postgresql/data \
-  -e POSTGRES_PASSWORD=password1234 postgres:9.4.26
+  --name "${DATABASE_CONTAINER_NAME}" \
+  -v "${DATABASE_VOLUME}":/var/lib/postgresql/data \
+  -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" postgres:"${POSTGRES_VERSION}"
 sleep 5
-docker exec coatrack-database psql -U postgres -c "create database coatrack;"
-docker exec coatrack-database psql -U postgres -c "create database ygg_config_server;"
-docker stop coatrack-database
+docker exec "${DATABASE_CONTAINER_NAME}" psql -U postgres -c "create database coatrack;"
+docker exec "${DATABASE_CONTAINER_NAME}" psql -U postgres -c "create database ygg_config_server;"
+docker stop "${DATABASE_CONTAINER_NAME}"
