7245e1fdd098c0e21409f84dead67c107e2ef828
Christoph Baier <baier@atb-bremen.de>
2022-01-03 14:32:09 +0100

    Introduced dynamic TAG environment variable, but commented it out, see the TODO for details.

--- docker-compose-setup/build-from-scratch.sh
+++ docker-compose-setup/build-from-scratch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Pulls latest tag from dockerhub repository. TODO Uncomment as soon as building local docker images is no longer required. Maybe "latest" could also be sufficient.
+#TAG=$(wget -q https://registry.hub.docker.com/v1/repositories/coatrack/coatrack-admin/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | tail -1)
+export TAG=2.0.0-SNAPSHOT
+
 ./uninstall.sh
 cd ..
 mvn package -DskipTests
