a63e7f1635212bb013e17fbfc23075217499ea9e
Bruno Silva <silva@atb-bremen.de>
2022-02-15 11:53:14 +0100

    Created service OauthUserAccountManagement to get every info related to the loggedInUser(name, loginName,company, email and token)

--- spring-boot/admin/src/main/java/eu/coatrack/admin/service/OauthUserAccountManagement.java
+++ spring-boot/admin/src/main/java/eu/coatrack/admin/service/OauthUserAccountManagement.java
@@ -1,11 +1,19 @@
 package eu.coatrack.admin.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OauthUserAccountManagement {
+
+    @Autowired
+    OAuth2AuthorizedClientService clientService;
 
     private OAuth2User getAttributesFromAuthentication() {
         OAuth2User loggedInUser = (OAuth2User) SecurityContextHolder
@@ -15,21 +23,30 @@ public class OauthUserAccountManagement {
         return loggedInUser;
     }
 
-    public String getLoginNameFromLoggedInUser(){
+    public String getLoginNameFromLoggedInUser() {
         return getAttributesFromAuthentication().getAttribute("login");
     }
 
-    public String getNameFromLoggedInUser(){
+    public String getNameFromLoggedInUser() {
         return getAttributesFromAuthentication().getAttribute("name");
     }
 
-    public String getCompanyFromLoggedInUser(){
+    public String getCompanyFromLoggedInUser() {
         return getAttributesFromAuthentication().getAttribute("company");
     }
 
-    public String getEmailFromLoggedInUser(){
+    public String getEmailFromLoggedInUser() {
         return getAttributesFromAuthentication().getAttribute("email");
     }
 
-
+    public String getTokenFromLoggedInUser() {
+        Authentication authentication = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        String clientRegistrationId = oauthToken.getAuthorizedClientRegistrationId();
+        OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(clientRegistrationId,
+                oauthToken.getName());
+        return client.getAccessToken().getTokenValue();
+    }
 }
