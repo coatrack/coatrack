65b7c59571f69a5d77885cf7ec58ba9ad2edcc76
Bruno Silva <silva@atb-bremen.de>
2022-02-15 12:32:58 +0100

    Add method to check the emails, in case email is set as private on GitHub

--- spring-boot/admin/src/main/java/eu/coatrack/admin/service/OauthUserAccountManagement.java
+++ spring-boot/admin/src/main/java/eu/coatrack/admin/service/OauthUserAccountManagement.java
@@ -1,6 +1,14 @@
 package eu.coatrack.admin.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.coatrack.config.github.GithubEmail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
@@ -8,9 +16,14 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class OauthUserAccountManagement {
+
+    private static final String GITHUB_API_EMAIL = "https://api.github.com/user/emails";
 
     @Autowired
     OAuth2AuthorizedClientService clientService;
@@ -35,8 +48,45 @@ public class OauthUserAccountManagement {
         return getAttributesFromAuthentication().getAttribute("company");
     }
 
-    public String getEmailFromLoggedInUser() {
-        return getAttributesFromAuthentication().getAttribute("email");
+    public String getEmailFromLoggedInUser() throws JsonProcessingException {
+        String email = getAttributesFromAuthentication().getAttribute("email");
+
+        // If the email is defined as private on GitHub (The Oauth2 will retrieve null),
+        // so the only way to retrieve is to make request with the Oauth Token
+        if (email == null || email.isEmpty()) {
+
+            // Initialize RestTemplate and HTTP Headers
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "token " + getTokenFromLoggedInUser());
+            HttpEntity<String> githubRequest = new HttpEntity(headers);
+
+            // Make the request to GitHub for the emails
+            ResponseEntity<String> userEmailsRequest = restTemplate.exchange(GITHUB_API_EMAIL, HttpMethod.GET,
+                    githubRequest, String.class);
+
+            email = GetPrimaryEmailFromLoggedInUser(userEmailsRequest);
+        }
+        return email;
+    }
+
+    private String GetPrimaryEmailFromLoggedInUser(ResponseEntity<String> userEmailsRequest) throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        List<GithubEmail> emailsList = objectMapper.
+                readValue(userEmailsRequest.getBody(),
+                        objectMapper
+                                .getTypeFactory()
+                                .constructCollectionType(List.class, GithubEmail.class));
+
+        for (GithubEmail userEmail : emailsList) {
+            if (userEmail.getVerified() == true && userEmail.getPrimary()) {
+                return userEmail.getEmail();
+            }
+        }
+        return null;
     }
 
     public String getTokenFromLoggedInUser() {
