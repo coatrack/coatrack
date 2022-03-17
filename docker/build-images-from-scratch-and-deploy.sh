f38836f67b173c48450ca2f0907f2fff9b70a105
Christoph Baier <baier@atb-bremen.de>
2022-03-17 16:17:21 +0100

    Added notes

--- docker/build-images-from-scratch-and-deploy.sh
+++ docker/build-images-from-scratch-and-deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# TODO: Add a mechanism to prevent trying to push in this case. -> Maybe make an extra push script.
+# TODO: Add a mechanism to prevent trying to push in this case. -> Maybe make an extra push script or boolean parameter.
 # TODO: All scripts should run with "set -e" and better logging of single steps.
 # TODO Replace sh scripts with bash scripts.
 # TODO: Make everything deployable locally via IntelliJ as well.
+# TODO: Adapt documentation.
+# TODO: Aggregate build scripts into one big script. The scripts themself are quite small. Maybe translate them to function.
 
 export PROJECT_DIR="${PWD}/.."
 export SPRING_COMPONENTS_DIR="${PROJECT_DIR}/spring-boot"
@@ -13,7 +15,7 @@ export DOCKER_COMPOSE_DEPLOYMENT_DIR="${DOCKER_DIR}/docker-compose-deployment"
 . "${DOCKER_COMPOSE_DEPLOYMENT_DIR}/stop-and-remove-all-containers.sh" # TODO Maybe add "-if-existent"?
 
 cd "${PROJECT_DIR}" || exit 1
-mvn package -DskipTests
+# TODO to be uncommented: mvn package -DskipTests
 
 . "${DOCKER_DIR}/build-and-push-all-docker-images.sh"
 . "${DOCKER_COMPOSE_DEPLOYMENT_DIR}/initialize-databases-if-necessary.sh"
--- docker/docker-compose-deployment/initialize-databases-if-necessary.sh
+++ docker/docker-compose-deployment/initialize-databases-if-necessary.sh
@@ -7,7 +7,8 @@ docker run --rm -d -p 5432:5432 \
   --name "${DATABASE_CONTAINER_NAME}" \
   -v "${DATABASE_VOLUME}":/var/lib/postgresql/data \
   -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" postgres:"${POSTGRES_VERSION}"
-sleep 5
+
+sleep 5 # Wait until database is set up.
 docker exec "${DATABASE_CONTAINER_NAME}" psql -U postgres -c "create database coatrack;"
 docker exec "${DATABASE_CONTAINER_NAME}" psql -U postgres -c "create database ygg_config_server;"
 docker stop "${DATABASE_CONTAINER_NAME}"
