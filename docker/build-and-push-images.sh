45352872569a11460dc04f0ed911312f3f22c69e
Christoph Baier <baier@atb-bremen.de>
2022-03-21 15:21:08 +0100

    Cleaned up and simplified docker image creation logic.

--- docker/build-and-push-images.sh
+++ docker/build-and-push-images.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 build-and-push-single-docker-image () {
-  MODULE_NAME=${1}
+  MODULE=${1}
   MODULE_VERSION=${2}
-  MODULE_DIR=${3}
 
-  echo "  Building version $MODULE_VERSION of module $MODULE_NAME in $MODULE_DIR"
-  docker build -f "${DOCKER_DIR}/Dockerfile" -t "coatrack/${MODULE_NAME}:${MODULE_VERSION}" --build-arg MODULE_VERSION="${MODULE_VERSION}" --build-arg MODULE_NAME="${MODULE_NAME}" --build-arg MODULE_DIR="${MODULE_DIR}" .
+  MODULE_DOCKER_IMAGE_NAME="coatrack/coatrack-${MODULE}:${MODULE_VERSION}"
+
+  echo "  Building version ${MODULE_VERSION} of module ${MODULE} in the directory coatrack/${MODULE}."
+  docker build -f "${DOCKER_DIR}/Dockerfile" -t "${MODULE_DOCKER_IMAGE_NAME}" --build-arg MODULE="${MODULE}" --build-arg MODULE_VERSION="${MODULE_VERSION}" .
 
   if [ "${IMAGE_PUSH_POLICY}" != "suppress-image-pushes" ]; then
-    echo "  Pushing coatrack/${MODULE_NAME}:${MODULE_VERSION} to Dockerhub."
-    docker push "coatrack/${MODULE_NAME}:${MODULE_VERSION}"
+    echo "  Pushing ${MODULE_DOCKER_IMAGE_NAME} to Dockerhub."
+    docker push "${MODULE_DOCKER_IMAGE_NAME}"
   fi
 }
 
@@ -22,9 +23,9 @@ cd "${PROJECT_DIR}" || exit 1
 mvn clean package -DskipTests
 
 CURRENT_MVN_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)"
-VERSION=${1:-$CURRENT_MVN_VERSION}
+MODULE_VERSION=${1:-$CURRENT_MVN_VERSION}
 echo "  Building docker images for version ${VERSION}"
 
 for MODULE in "admin" "proxy" "config-server"; do
-  build-and-push-single-docker-image "coatrack-${MODULE}" "${VERSION}" "spring-boot/${MODULE}"
+  build-and-push-single-docker-image "${MODULE}" "${MODULE_VERSION}"
 done
