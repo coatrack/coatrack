a2ddc9f5483c95be122cbf5ecbbf440d9a06974a
Christoph Baier <baier@atb-bremen.de>
2022-03-31 10:16:04 +0200

    Fixed build problem by adapting variable names.

--- docker/build-and-push-images.sh
+++ docker/build-and-push-images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 build-and-push-single-docker-image () {
-  MODULE=${1}
-  MODULE_DOCKER_IMAGE_NAME="coatrack/coatrack-${MODULE}:${PROJECT_VERSION}"
+  COATRACK_MODULE=${1}
+  MODULE_DOCKER_IMAGE_NAME="coatrack/coatrack-${COATRACK_MODULE}:${COATRACK_VERSION}"
 
-  echo "  Building version ${PROJECT_VERSION} of module ${MODULE} in the directory coatrack/${MODULE}."
-  docker build -f "${DOCKER_DIR}/Dockerfile" -t "${MODULE_DOCKER_IMAGE_NAME}" --build-arg MODULE="${MODULE}" --build-arg PROJECT_VERSION="${PROJECT_VERSION}" .
+  echo "  Building version ${COATRACK_VERSION} of module ${COATRACK_MODULE} in the directory coatrack/${COATRACK_MODULE}."
+  docker build -f "${DOCKER_DIR}/Dockerfile" -t "${MODULE_DOCKER_IMAGE_NAME}" --build-arg COATRACK_MODULE="${COATRACK_MODULE}" --build-arg COATRACK_VERSION="${COATRACK_VERSION}" .
 
   if [ "${IMAGE_PUSH_POLICY}" != "suppress-image-pushes" ]; then
     echo "  Pushing ${MODULE_DOCKER_IMAGE_NAME} to Dockerhub."
@@ -16,13 +16,13 @@ build-and-push-single-docker-image () {
 export IMAGE_PUSH_POLICY="$1"
 source "environment-variables.sh"
 cd "${PROJECT_DIR}" || exit 1
-export PROJECT_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)"
+export COATRACK_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)"
 
 printf "\nBuilding CoatRack module docker images and pushing them into Dockerhub.\n"
 echo "  Building jar files of CoatRack modules from source."
 mvn clean package -DskipTests
 
-echo "  Building docker images for version ${PROJECT_VERSION}"
-for MODULE in "admin" "proxy" "config-server"; do
-  build-and-push-single-docker-image "${MODULE}"
+echo "  Building docker images for version ${COATRACK_VERSION}"
+for COATRACK_MODULE in "admin" "proxy" "config-server"; do
+  build-and-push-single-docker-image "${COATRACK_MODULE}"
 done
