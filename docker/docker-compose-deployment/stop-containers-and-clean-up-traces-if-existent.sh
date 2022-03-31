3da5b82104da7908be60aeaf7be399baf4f97872
Christoph Baier <baier@atb-bremen.de>
2022-03-31 09:55:52 +0200

    Replaced 'proxy' by 'gateway' in variable names and values.

--- docker/docker-compose-deployment/stop-containers-and-clean-up-traces-if-existent.sh
+++ docker/docker-compose-deployment/stop-containers-and-clean-up-traces-if-existent.sh
@@ -6,7 +6,7 @@ cd "${DOCKER_COMPOSE_DEPLOYMENT_DIR}" || exit 1
 printf "\nCleaning up by removing currently running containers and volumes if existent.\n"
 
 docker-compose down
-docker volume rm "${DATABASE_VOLUME}" "${PROXY_CONFIG_FILES_VOLUME}"
+docker volume rm "${DATABASE_VOLUME}" "${GATEWAY_CONFIG_FILES_VOLUME}"
 
 for MODULE in "admin" "proxy" "config-server"; do
   echo "  Deleting module coatrack-${MODULE}"
