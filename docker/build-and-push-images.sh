76757fc836d857cb30528d91d50a91e4ed2c96a9
Christoph Baier <baier@atb-bremen.de>
2022-03-31 14:22:37 +0200

    Separated and extracted build and push logic.

--- docker/build-and-push-images.sh
+++ docker/build-and-push-images.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-build-and-push-single-docker-image () {
-  COATRACK_MODULE=${1}
-  MODULE_DOCKER_IMAGE_NAME="coatrack/coatrack-${COATRACK_MODULE}:${COATRACK_VERSION}"
-
-  echo "  Building version ${COATRACK_VERSION} of module ${COATRACK_MODULE} in the directory coatrack/${COATRACK_MODULE}."
-  docker build -f "${DOCKER_DIR}/Dockerfile" -t "${MODULE_DOCKER_IMAGE_NAME}" --build-arg COATRACK_MODULE="${COATRACK_MODULE}" --build-arg COATRACK_VERSION="${COATRACK_VERSION}" .
-
-  if [ "${IMAGE_PUSH_POLICY}" != "suppress-image-pushes" ]; then
-    echo "  Pushing ${MODULE_DOCKER_IMAGE_NAME} to Dockerhub."
-    docker push "${MODULE_DOCKER_IMAGE_NAME}"
-  fi
-}
-
 export IMAGE_PUSH_POLICY="$1"
 source "environment-variables.sh"
 cd "${PROJECT_DIR}" || exit 1
-export COATRACK_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)"
 
 # TODO This statement is wrong when this script is called by build-and-deploy-images-locally.sh.
 #   TODO The "IMAGE_PUSH_POLICY" is confusing anyhow. I refactor that. Maybe extract script like "build-images",
@@ -27,5 +13,6 @@ mvn clean package -DskipTests
 
 echo "  Building docker images for version ${COATRACK_VERSION}"
 for COATRACK_MODULE in "admin" "proxy" "config-server"; do
-  build-and-push-single-docker-image "${COATRACK_MODULE}"
+  build-single-docker-image "${COATRACK_MODULE}"
+  push-single-docker-image "${COATRACK_MODULE}"
 done
--- docker/environment-variables.sh
+++ docker/environment-variables.sh
@@ -4,3 +4,23 @@ export PROJECT_DIR="${PWD}/.."
 export SPRING_COMPONENTS_DIR="${PROJECT_DIR}/spring-boot"
 export DOCKER_DIR="${PROJECT_DIR}/docker"
 export DOCKER_COMPOSE_DEPLOYMENT_DIR="${DOCKER_DIR}/docker-compose-deployment"
+cd "${PROJECT_DIR}" || exit 1
+export COATRACK_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)"
+
+build-single-docker-image () {
+  COATRACK_MODULE=${1}
+  MODULE_DOCKER_IMAGE_NAME="coatrack/coatrack-${COATRACK_MODULE}:${COATRACK_VERSION}"
+
+  echo "  Building version ${COATRACK_VERSION} of module ${COATRACK_MODULE} in the directory coatrack/${COATRACK_MODULE}."
+  docker build -f "${DOCKER_DIR}/Dockerfile" -t "${MODULE_DOCKER_IMAGE_NAME}" --build-arg COATRACK_MODULE="${COATRACK_MODULE}" --build-arg COATRACK_VERSION="${COATRACK_VERSION}" .
+}
+
+push-single-docker-image () {
+  COATRACK_MODULE=${1}
+  MODULE_DOCKER_IMAGE_NAME="coatrack/coatrack-${COATRACK_MODULE}:${COATRACK_VERSION}"
+
+  if [ "${IMAGE_PUSH_POLICY}" != "suppress-image-pushes" ]; then
+    echo "  Pushing ${MODULE_DOCKER_IMAGE_NAME} to Dockerhub."
+    docker push "${MODULE_DOCKER_IMAGE_NAME}"
+  fi
+}
