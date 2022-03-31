142dfa8c9c987caba7bece09257149c3845141df
Christoph Baier <baier@atb-bremen.de>
2022-03-31 16:04:35 +0200

    Refactoring.

--- docker/build-and-push-images.sh
+++ docker/build-and-push-images.sh
@@ -2,6 +2,15 @@
 
 . "environment-variables.sh"
 
+push-single-docker-image () {
+  COATRACK_MODULE=${1}
+  MODULE_DOCKER_IMAGE_NAME="coatrack/coatrack-${COATRACK_MODULE}:${COATRACK_VERSION}"
+
+  cd "${PROJECT_DIR}" || exit 1
+  echo "  Pushing ${MODULE_DOCKER_IMAGE_NAME} to Dockerhub."
+  docker push "${MODULE_DOCKER_IMAGE_NAME}"
+}
+
 echo "  Building und pushing docker images for version ${COATRACK_VERSION}"
 for COATRACK_MODULE in "admin" "proxy" "config-server"; do
   cd "${DOCKER_DIR}" || exit 1
--- docker/environment-variables.sh
+++ docker/environment-variables.sh
@@ -10,10 +10,8 @@ cd "${DOCKER_COMPOSE_DEPLOYMENT_DIR}" || exit 1
 
 cd "${PROJECT_DIR}" || exit 1
 export COATRACK_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)"
-
 printf "\nBuilding CoatRack module docker images.\n"
 echo "  Building jar files of CoatRack modules from source."
-cd "${PROJECT_DIR}" || exit 1
 mvn clean package -DskipTests jib:dockerBuild
 
 build-single-docker-image () {
@@ -23,13 +21,4 @@ build-single-docker-image () {
   cd "${PROJECT_DIR}" || exit 1
   echo "  Building version ${COATRACK_VERSION} of module ${COATRACK_MODULE} in the directory coatrack/${COATRACK_MODULE}."
   docker build -f "${DOCKER_DIR}/Dockerfile" -t "${MODULE_DOCKER_IMAGE_NAME}" --build-arg COATRACK_MODULE="${COATRACK_MODULE}" --build-arg COATRACK_VERSION="${COATRACK_VERSION}" .
-}
-
-push-single-docker-image () {
-  COATRACK_MODULE=${1}
-  MODULE_DOCKER_IMAGE_NAME="coatrack/coatrack-${COATRACK_MODULE}:${COATRACK_VERSION}"
-
-  cd "${PROJECT_DIR}" || exit 1
-  echo "  Pushing ${MODULE_DOCKER_IMAGE_NAME} to Dockerhub."
-  docker push "${MODULE_DOCKER_IMAGE_NAME}"
 }
