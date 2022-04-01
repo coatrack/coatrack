46b975d52729fe18d0247ee4ed80e6eafae98f21
Christoph Baier <baier@atb-bremen.de>
2022-04-01 13:42:19 +0200

    Fixed follow-up configuration bug and renamed databases.

--- docker/docker-compose-deployment/initialize-databases-if-necessary.sh
+++ docker/docker-compose-deployment/initialize-databases-if-necessary.sh
@@ -15,9 +15,9 @@ docker run --rm -d \
 echo "  Waiting a few seconds until database is ready."
 sleep 5
 
-echo "  Create databases 'coatrack' and 'ygg_config_server' in PostgreSQL."
-docker exec "${DATABASE_CONTAINER_NAME}" psql -U postgres -c "create database coatrack;"
-docker exec "${DATABASE_CONTAINER_NAME}" psql -U postgres -c "create database ygg_config_server;"
+echo "  Create databases '${POSTGRES_DATABASE_NAME_FOR_WEB_APPLICATION}' and '${POSTGRES_DATABASE_NAME_FOR_CONFIG_SERVER}' in PostgreSQL."
+docker exec "${DATABASE_CONTAINER_NAME}" psql -U postgres -c "create database ${POSTGRES_DATABASE_NAME_FOR_WEB_APPLICATION};"
+docker exec "${DATABASE_CONTAINER_NAME}" psql -U postgres -c "create database ${POSTGRES_DATABASE_NAME_FOR_CONFIG_SERVER};"
 
 echo "  Stopping initialized database."
 docker stop "${DATABASE_CONTAINER_NAME}"
