b8a7c519efbe1043b7203705c18d89f11f8efc2c
Bruno Silva <silva@atb-bremen.de>
2022-04-11 18:17:13 +0200

    Adapted ResourceServerTokenServices with CustomUserInfoTokenServices

--- spring-boot/admin/src/main/java/eu/coatrack/admin/GitHubSecurityConfig.java
+++ spring-boot/admin/src/main/java/eu/coatrack/admin/GitHubSecurityConfig.java
@@ -20,14 +20,29 @@ package eu.coatrack.admin;
  * #L%
  */
 
+import eu.coatrack.admin.security.CustomUserInfoTokenServices;
+import eu.coatrack.admin.security.PublicApiTokenAccessFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitHubSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] PERMITALL_RESOURCE_LIST = new String[]{"/bower_components/**", "/catchPaymentResponse", "/json/**", "/login", "/", "/403", "/registerUser", "/callback", "/fonts/**", "/webjars/**", "/robots.txt", "/assets/**", "/images/**"};
+
+    @Autowired
+    private OAuth2ClientProperties oAuth2ClientProperties;
+
+    @Autowired
+    public ResourceServerTokenServices tokenServices() {
+        CustomUserInfoTokenServices tokenServices = new CustomUserInfoTokenServices(oAuth2ClientProperties.getProvider().get("github").getUserInfoUri(), oAuth2ClientProperties.getRegistration().get("github").getClientId());
+        return tokenServices;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,6 +52,7 @@ public class GitHubSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PERMITALL_RESOURCE_LIST).permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilterBefore(new PublicApiTokenAccessFilter(tokenServices()), AbstractPreAuthenticatedProcessingFilter.class)
                 .logout().logoutUrl("/logout").logoutSuccessUrl("/").deleteCookies("XSRF-TOKEN", "auth_code", "JSESSIONID").invalidateHttpSession(true).clearAuthentication(true).permitAll()
                 .and()
                 .oauth2Login()
---
+++ spring-boot/admin/src/main/java/eu/coatrack/admin/security/CustomUserInfoTokenServices.java
@@ -0,0 +1,51 @@
+package eu.coatrack.admin.security;
+
+/*-
+ * #%L
+ * coatrack-admin
+ * %%
+ * Copyright (C) 2013 - 2022 Corizon | Institut für angewandte Systemtechnik Bremen GmbH (ATB)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+public class CustomUserInfoTokenServices implements ResourceServerTokenServices {
+
+    private final String userInfoEndpointUrl;
+    private final String clientId;
+
+    public CustomUserInfoTokenServices(String userInfoEndpointUrl, String clientId) {
+        this.userInfoEndpointUrl = userInfoEndpointUrl;
+        this.clientId = clientId;
+    }
+
+    @Override
+    public OAuth2Authentication loadAuthentication(String accessToken)
+            throws AuthenticationException, InvalidTokenException {
+        UserInfoTokenServices userInfoTokenServices = new UserInfoTokenServices(userInfoEndpointUrl, clientId);
+        return userInfoTokenServices.loadAuthentication(accessToken);
+    }
+
+    @Override
+    public OAuth2AccessToken readAccessToken(String accessToken) {
+        throw new UnsupportedOperationException("Not supported: read access token");
+    }
+}
