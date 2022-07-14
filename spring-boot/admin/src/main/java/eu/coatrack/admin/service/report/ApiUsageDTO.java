7f8c29aaafd3f445190ed94b81c8c300c512003b
MortenBellmannATB <bellmann@atb-bremen.de>
2022-07-14 07:37:07 +0200

    Add Getter and Setter to enable mocking of dto

--- spring-boot/admin/src/main/java/eu/coatrack/admin/service/report/ApiUsageDTO.java
+++ spring-boot/admin/src/main/java/eu/coatrack/admin/service/report/ApiUsageDTO.java
@@ -3,15 +3,19 @@ package eu.coatrack.admin.service.report;
 import eu.coatrack.api.ServiceApi;
 import eu.coatrack.api.User;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public class ApiUsageDTO {
-        public final ServiceApi service;
-        public final User consumer;
-        public final Date from;
-        public final Date until;
-        public final boolean considerOnlyPaidCalls;
-        public final boolean isForConsumer;
+        private final ServiceApi service;
+        private final User consumer;
+        private final Date from;
+        private final Date until;
+        private final boolean considerOnlyPaidCalls;
+        private final boolean isForConsumer;
 }
