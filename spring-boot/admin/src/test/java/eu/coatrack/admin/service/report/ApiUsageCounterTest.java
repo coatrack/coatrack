0f943fb9db2bffb94f57023a68deaef81a79fdd9
MortenBellmannATB <bellmann@atb-bremen.de>
2022-08-04 09:48:12 +0200

    Add ApiUsageCounterTest

--- spring-boot/admin/src/test/java/eu/coatrack/admin/service/report/ApiUsageCounterTest.java
+++ spring-boot/admin/src/test/java/eu/coatrack/admin/service/report/ApiUsageCounterTest.java
@@ -1,4 +1,57 @@
 package eu.coatrack.admin.service.report;
 
+import eu.coatrack.admin.model.repository.MetricsAggregationCustomRepository;
+import eu.coatrack.api.MetricType;
+import eu.coatrack.api.ServiceAccessPaymentPolicy;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
+
+import static eu.coatrack.admin.service.report.ReportDummyFactory.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
 public class ApiUsageCounterTest {
+
+    @Autowired
+    private ApiUsageCounter apiUsageCounter;
+
+    private MetricsAggregationCustomRepository metricsAggregationCustomRepository;
+
+    public ApiUsageCounterTest() {
+        metricsAggregationCustomRepository = mock(MetricsAggregationCustomRepository.class);
+
+        doReturn(getMetricResultList()).when(metricsAggregationCustomRepository).getUsageApiConsumer(
+                any(MetricType.class), anyLong(), anyString(), anyLong(), any(Date.class), any(Date.class)
+        );
+
+        doReturn(getMetricResultList()).when(metricsAggregationCustomRepository).getUsageApiConsumer(
+                any(MetricType.class), anyLong(), anyString(), anyLong(), any(Date.class), any(Date.class), anyBoolean()
+        );
+
+
+        apiUsageCounter = new ApiUsageCounter(metricsAggregationCustomRepository);
+    }
+
+    @Test
+    public void countMonthly() {
+        ApiUsageDTO apiUsageDTO = getApiUsageDTODummy(ServiceAccessPaymentPolicy.MONTHLY_FEE);
+        CallCount result = apiUsageCounter.count(apiUsageDTO);
+        assertFalse(result.isEmpty());
+        assertEquals(6L, result.getMonthlyBilledCalls());
+    }
+
+    @Test
+    public void countEntryPoint() {
+        ApiUsageDTO apiUsageDTO = getApiUsageDTODummy(ServiceAccessPaymentPolicy.WELL_DEFINED_PRICE);
+        CallCount result = apiUsageCounter.count(apiUsageDTO);
+        assertFalse(result.isEmpty());
+        assertEquals(6L, result.getCallsByEntryPoint(getEntryPointDummys().get(0)));
+
+    }
+
+
 }
