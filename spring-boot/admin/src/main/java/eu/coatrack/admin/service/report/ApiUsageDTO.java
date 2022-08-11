974ee34d0be7f98666034d009a647cc9cd6ea4e9
MortenBellmannATB <bellmann@atb-bremen.de>
2022-08-11 16:21:11 +0200

    Add NoArgConstructor

--- spring-boot/admin/src/main/java/eu/coatrack/admin/service/report/ApiUsageDTO.java
+++ spring-boot/admin/src/main/java/eu/coatrack/admin/service/report/ApiUsageDTO.java
@@ -4,18 +4,20 @@ import eu.coatrack.api.ServiceApi;
 import eu.coatrack.api.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class ApiUsageDTO {
-        private final ServiceApi service;
-        private final User consumer;
-        private final Date from;
-        private final Date until;
-        private final boolean considerOnlyPaidCalls;
-        private final boolean isForConsumer;
+        private ServiceApi service;
+        private User consumer;
+        private Date from;
+        private Date until;
+        private boolean considerOnlyPaidCalls;
+        private boolean isForConsumer;
 }
